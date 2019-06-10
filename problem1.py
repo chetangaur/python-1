665eb0bbb75537e838a98e8e294c86d0253048ac
Aditikabra20 <maheshwariaditi321@gmail.com>
2019-06-10 08:39:40 +0530

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,6 +1,8 @@
-#! /usr/bin/pytho36
+#! /usr/bin/python36
+import datetime
+x=datetime.datetime.now().year
 name=input("what is your name-")
 age=int(input("age-"))
 
-year=str((2019-age)+95)
+year=str((x-age)+95)
 print(name + " will be 95 year old in " +year) 
