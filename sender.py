f8cd9a91892184e7739a23319c09322c662957da
ARPIT443 <arpit443@gmail.com>
2020-10-01 10:08:12 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -10,7 +10,7 @@ s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 while 4>2: #while loop
 	msg=input("enter msg u want to send..")
-	nmsg=msg.encode('ascii') #change string into byte-like form
+	nmsg=msg.encode('ascii') # change string into byte-like form
 # sending data to receiver
 	s.sendto(nmsg,(recv_ip,recv_port))
 # receive data form rece. 
